3a645a317a79c1c79268c87a08838c462c329461
Wendy <alteredco@gmail.com>
2019-10-19 17:31:32 +0200

    Updates

---
+++ precache-manifest.d1fe61e7db733e57762b88306e9f12b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d72de5abc33582659d720f779e75ef6",
+    "url": "/course_directory/index.html"
+  },
+  {
+    "revision": "27529920815017b3e612",
+    "url": "/course_directory/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "fb9f1639143991a3d556",
+    "url": "/course_directory/static/js/2.d72e90c6.chunk.js"
+  },
+  {
+    "revision": "27529920815017b3e612",
+    "url": "/course_directory/static/js/main.88558808.chunk.js"
+  },
+  {
+    "revision": "5055097b733a98102344",
+    "url": "/course_directory/static/js/runtime-main.c3665c7e.js"
+  }
+]);
